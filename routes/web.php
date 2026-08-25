649a4449c8ded5d163e290eb6bcc0d6cea7b9fe4
yama103 <yama05080312@gmail.com>
2026-08-25 05:41:39 +0000

    Configure Laravel routes and proxy support

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/jobs', function () {
     return view('jobs');
-});
+})->name('jobs');
 
 Route::get('/job-details', function () {
     return view('job-details');
-});
+})->name('job-details');
 
 Route::get('/about-us', function () {
     return view('about-us');
-});
+})->name('about-us');
 
 Route::get('/contact-us', function () {
     return view('contact-us');
-});
+})->name('contact-us');
