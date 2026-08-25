8e4ab202f04b77461b2ea60a2f148588924ed074
yama103 <yama05080312@gmail.com>
2026-08-25 06:04:42 +0000

    Convert footer component to Laravel

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,71 +1,67 @@
  <footer class="footer">
-      <div class="container">
-        <div class="footer__wrapper">
-          <div class="footer__top">
-            <div class="footer__col footer__col--brand">
-              <div class="footer__logo">
-                <img src="images/sitelogo.svg" alt="" />
-                <span class="footer__title">Job</span>
-              </div>
-              <p class="footer__desc">
-                Quis enim pellentesque viverra tellus eget malesuada facilisis.
-                Congue nibh vivamus aliquet nunc mauris du
-              </p>
-            </div>
+     <div class="container">
+         <div class="footer__wrapper">
+             <div class="footer__top">
+                 <div class="footer__col footer__col--brand">
+                     <div class="footer__logo">
+                         <img src="/images/sitelogo.svg" alt="" />
+                         <span class="footer__title">Job</span>
+                     </div>
+                     <p class="footer__desc">
+                         Quis enim pellentesque viverra tellus eget malesuada facilisis.
+                         Congue nibh vivamus aliquet nunc mauris du
+                     </p>
+                 </div>
 
-            <div class="footer__col footer__col--company">
-              <h3 class="footer__title">Company</h3>
-              <ul class="footer__list">
-                <li><a href="#" class="footer__link">About Us</a></li>
-                <li><a href="#" class="footer__link">Our Team</a></li>
-                <li><a href="#" class="footer__link">Partners</a></li>
-                <li><a href="#" class="footer__link">For Candidates</a></li>
-                <li><a href="#" class="footer__link">For Employers</a></li>
-              </ul>
-            </div>
-            <div class="footer__col footer__col--category">
-              <h3 class="footer__title">Job Categories</h3>
-              <ul class="footer__list">
-                <li><a href="#" class="footer__link">Telecomunications</a></li>
-                <li><a href="#" class="footer__link">Hotels & Tourism</a></li>
-                <li><a href="#" class="footer__link">Construction</a></li>
-                <li><a href="#" class="footer__link">Education</a></li>
-                <li><a href="#" class="footer__link">Financial Services</a></li>
-              </ul>
-            </div>
-            <div class="footer__col footer__col--news">
-              <h3 class="footer__title">Newsletter</h3>
+                 <div class="footer__col footer__col--company">
+                     <h3 class="footer__title">Company</h3>
+                     <ul class="footer__list">
+                         <li><a href="{{ route('about-us') }}" class="footer__link">About Us</a></li>
+                         <li><a href="#" class="footer__link">Our Team</a></li>
+                         <li><a href="#" class="footer__link">Partners</a></li>
+                         <li><a href="#" class="footer__link">For Candidates</a></li>
+                         <li><a href="#" class="footer__link">For Employers</a></li>
+                     </ul>
+                 </div>
+                 <div class="footer__col footer__col--category">
+                     <h3 class="footer__title">Job Categories</h3>
+                     <ul class="footer__list">
+                         <li><a href="#" class="footer__link">Telecomunications</a></li>
+                         <li><a href="#" class="footer__link">Hotels & Tourism</a></li>
+                         <li><a href="#" class="footer__link">Construction</a></li>
+                         <li><a href="#" class="footer__link">Education</a></li>
+                         <li><a href="#" class="footer__link">Financial Services</a></li>
+                     </ul>
+                 </div>
+                 <div class="footer__col footer__col--news">
+                     <h3 class="footer__title">Newsletter</h3>
 
-              <p class="footer__news-desc">
-                Eu nunc pretium vitae platea. Non netus elementum vulputate
-              </p>
-              <form class="footer__form">
-                <input
-                  class="footer__input"
-                  id="newsletter-email"
-                  type="email"
-                  placeholder="Email Address"
-                />
-                <button type="submit" class="button footer__button">
-                  Subscribe now
-                </button>
-              </form>
-            </div>
-          </div>
+                     <p class="footer__news-desc">
+                         Eu nunc pretium vitae platea. Non netus elementum vulputate
+                     </p>
+                     <form class="footer__form">
+                         <input class="footer__input" id="newsletter-email" type="email"
+                             placeholder="Email Address" />
+                         <button type="submit" class="button footer__button">
+                             Subscribe now
+                         </button>
+                     </form>
+                 </div>
+             </div>
 
-          <div class="footer__bottom">
-            <div class="footer__addition footer__addition--mobile">
-              <a href="#" class="footer__privacy">Privacy Policy</a>
-              <a href="#" class="footer__terms">Terms & Conditions</a>
-            </div>
-            <p class="footer__copy">
-              © Copyright Job Portal 2024. Designed by Figma.guru
-            </p>
-            <div class="footer__addition footer__addition--desktop">
-              <a href="#" class="footer__privacy">Privacy Policy</a>
-              <a href="#" class="footer__terms">Terms & Conditions</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
+             <div class="footer__bottom">
+                 <div class="footer__addition footer__addition--mobile">
+                     <a href="#" class="footer__privacy">Privacy Policy</a>
+                     <a href="#" class="footer__terms">Terms & Conditions</a>
+                 </div>
+                 <p class="footer__copy">
+                     © Copyright Job Portal 2024. Designed by Figma.guru
+                 </p>
+                 <div class="footer__addition footer__addition--desktop">
+                     <a href="#" class="footer__privacy">Privacy Policy</a>
+                     <a href="#" class="footer__terms">Terms & Conditions</a>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </footer>
