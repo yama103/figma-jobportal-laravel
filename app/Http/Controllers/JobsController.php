167599e0eea10eccf555e451d2b65f0e54302704
yama103 <yama05080312@gmail.com>
2026-08-25 07:55:20 +0000

    Refactor jobs page with controller data

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -6,6 +6,64 @@ class JobsController extends Controller
 {
     public function index()
     {
-        return view('jobs');
+        $title = 'Available Jobs';
+        $description = 'Find your next career opportunity.';
+
+        $jobs = [
+            [
+                'title' => 'Forward Security Director',
+                'company' => 'Bauch, Schuppe and Schulist Co',
+                'category' => 'Hotels & Tourism',
+                'type' => 'Full time',
+                'salary' => '$40000-$42000',
+                'location' => 'New-York, USA',
+            ],
+            [
+                'title' => 'Regional Creative Facilitator',
+                'company' => 'Wisozk - Becker Co',
+                'category' => 'Media',
+                'type' => 'Part time',
+                'salary' => '$28000-$32000',
+                'location' => 'Los-Angeles, USA',
+            ],
+            [
+                'title' => 'Internal Integration Planner',
+                'company' => 'Mraz, Quigley and Feest Inc.',
+                'category' => 'Construction',
+                'type' => 'Full time',
+                'salary' => '$48000-$50000',
+                'location' => 'Texas, USA',
+            ],
+            [
+                'title' => 'District Intranet Director',
+                'company' => 'VonRueden - Weber Co',
+                'category' => 'Commerce',
+                'type' => 'Full time',
+                'salary' => '$42000-$48000',
+                'location' => 'Florida, USA',
+            ],
+            [
+                'title' => 'Corporate Tactics Facilitator',
+                'company' => 'Cormier, Turner and Flatley Inc',
+                'category' => 'Commerce',
+                'type' => 'Full time',
+                'salary' => '$38000-$40000',
+                'location' => 'Boston, USA',
+            ],
+            [
+                'title' => 'Forward Accounts Consultant',
+                'company' => 'Miller Group',
+                'category' => 'Financial services',
+                'type' => 'Full time',
+                'salary' => '$45000-$48000',
+                'location' => 'Boston, USA',
+            ],
+        ];
+
+        return view('jobs', [
+            'title' => $title,
+            'description' => $description,
+            'jobs' => $jobs,
+        ]);
     }
 }
